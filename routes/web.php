388b120c10497bd089a878b4db66ba667a0b444a
pawelztef <pawelztef@gmail.com>
2018-01-02 09:36:45 +0000

    first stage of auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('backend_layout');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
