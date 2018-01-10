181756ac9996c80b2720dac17aaa04ae0e26d836
pawelztef <pawelztef@gmail.com>
2018-01-10 09:26:20 +0000

    Global variables implemented in AppServiceProvider and can be used in all views even in 404

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
 
   public function index()
   {
-    return view('layouts.backend_layout');
+    return view('back.dashboard');
   }
 
 }
--- resources/views/back/dashboard.blade.php
+++ resources/views/back/dashboard.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.backend_layout')
-
 @section('content')
-<div class="container">
+  <section class="content-header">
+    <h1> <i class="small fa fa-chevron-right"></i> Dashboard</h1>
+  </section>
+  <section class="content">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+      <div class="col-md-12">
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">Users Table</h3>
+          </div>
+          <img src="/storage/logo_images/{{$site_settings->logo_image}}" alt="">
         </div>
+      </div>
     </div>
-</div>
+  </section>
 @endsection
